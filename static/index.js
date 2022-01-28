e18acd3a4636f301c7124dd413f3ee143503a1a5
xseele <91596058+xseele@users.noreply.github.com>
2022-01-28 15:03:25 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -440,10 +440,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
 
         if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 8) return '你对于wfx的厨力也不过如此';
+        if (cps <= 10)  return '您就是wfx本人罢';
+        if (cps <= 15) return '星人类诞生了';
+        return 'Ka，，Kami哒？';
     }
 
     function toStr(obj) {
